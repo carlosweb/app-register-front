1a50ea739ee63742f10b6023eecc954e31aaae28
Carlos Eduardo <kaduinfo@gmail.com>
2024-08-29 11:31:54 -0300

    feat: BaseUrl updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://app-register-two.vercel.app/'
+    baseURL: 'https://app-register-two.vercel.app/users'
 })
 
 export default api
